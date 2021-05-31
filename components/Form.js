82afd664c4bf4c7e28d25c2e4a16eac8a687f80c
jbuyu <jbuyu@ymail.com>
2021-05-31 15:29:33 +0300

    adds query string on search

--- components/Form.js
+++ components/Form.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 import { MicrophoneIcon, SearchIcon } from "@heroicons/react/outline";
+const router = useRouter();
 export const Form = () => {
+  const searchInputRef = useRef(null);
+  const search = (e) => {
+    e.preventDefault();
+    const term = searchInputRef.current.value;
+
+    if (!term) return;
+    router.push(`/search?term=${term}`);
+  };
   return (
     <form className="flex flex-col items-center mt-44 flex-grow w-5/6">
       <Image
@@ -13,11 +23,17 @@ export const Form = () => {
       />
       <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
         <SearchIcon className="h-5 mr-3 text-gray-500 " />
-        <input type="text" className="focus:outline-none flex-grow" />
+        <input
+          ref={searchInputRef}
+          type="text"
+          className="focus:outline-none flex-grow"
+        />
         <MicrophoneIcon className="h-5" />
       </div>
       <div className="flex flex-col w-1/2 spae-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
-        <button className="btn">Google Search</button>
+        <button onClick={search} className="btn">
+          Google Search
+        </button>
         <button className="btn">I'm feeling Lucky</button>
       </div>
     </form>
