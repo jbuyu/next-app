c2b978cec4c0c04e7b7fb193416394d55b91d898
jbuyu <jbuyu@ymail.com>
2021-05-31 17:28:02 +0300

    changes on icon

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,10 @@
-import React from "react";
-import Image from 'next/image'
+import React, { useRef } from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { MicrophoneIcon, XIcon } from "@heroicons/react/solid";
 function Header() {
+  const router = useRouter();
+  const searchInputRef = useRef();
   return (
     <header>
       <Image
@@ -8,7 +12,25 @@ function Header() {
         height={64}
         width={124}
         className="cursor-pointer"
+        onClick={() => router.push("/")}
       />
+      <form className="flex px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg max-w-3xl items-center">
+        <input
+          className="flex-grow w-full focus:outline-none"
+          ref={searchInputRef}
+          type="text"
+        />
+        <XIcon
+          className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125
+          "
+          onClick={() => (searchInputRef.current.value = "")}
+        />
+        <MicrophoneIcon
+        className="mr-3 h-6 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300"
+
+
+/>
+      </form>
     </header>
   );
 }
