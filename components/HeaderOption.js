f83a873416978bce3b5fb284d09189f21720e22c
jbuyu <jbuyu@ymail.com>
2021-06-01 11:07:03 +0300

    inline styling padding on items

--- components/HeaderOption.js
+++ components/HeaderOption.js
@@ -1,10 +1,14 @@
 function HeaderOption({ Icon, title, selected }) {
-  return(
-      <div className={`flex`} >
-          <Icon className="h-4"   />
-          <p className="hidden sm:inline-flex">{title}</p>
-      </div>
-  )
+  return (
+    <div
+      className={`flex items-center space-x-1 pb-3 cursor-pointer
+       border-b-4 border-transparent hover:text-blue-500 
+       hover:border-blue-500 ${selected && "text-blue-500 border-blue-500"} `}
+    >
+      <Icon className="h-4" />
+      <p className="hidden sm:inline-flex">{title}</p>
+    </div>
+  );
 }
 
 export default HeaderOption;
