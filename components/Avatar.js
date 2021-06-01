d4c1bd6a2b9ee0f91832abca79be52fa0d257b86
jbuyu <jbuyu@ymail.com>
2021-06-01 09:23:28 +0300

    passes classname down to component

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,14 @@
 import React from "react";
-import Image from 'next/image'
-export const Avatar = ({ url }) => {
-  return <img className="rounded-full h-10 cursor-pointer transition duration-150 transform hover:scale-110" loading="lazy" src={url} alt="profile_piq" height='48' width='48'  />;
+import Image from "next/image";
+export const Avatar = ({ url, className }) => {
+  return (
+    <img
+      className={`rounded-full h-10 cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
+      loading="lazy"
+      src={url}
+      alt="profile_piq"
+      height="48"
+      width="48"
+    />
+  );
 };
