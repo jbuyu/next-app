95a039cac03e7bba8c1853b38eb2067e4db7f863
jbuyu <jbuyu@ymail.com>
2021-06-02 11:59:42 +0300

    setting up inoming response

--- pages/search.js
+++ pages/search.js
@@ -1,18 +1,23 @@
 import React from "react";
 import Head from "next/head";
 import Header from "../components/Header";
-import Response from '../Response'
-function Search({results}) {
+import Response from "../Response";
+import { useRouter } from "next/router";
+import SearchResults from "../components/SearchResults";
+function Search({ results }) {
   //   console.log(process.env.REACT_APP_API_KEY, process.env.REACT_APP_CONTEXT_KEY);
   //   console.log(process.env.REACT_APP_API_kEY);
   //   console.log(process.env.REACT_APP_CONTEXT_kEY);
-  console.log(results)
+  const router = useRouter();
+  console.log(results);
   return (
     <div>
       <Head>
-        <title>Search Results</title>
+        <title>{router.query.term} - Google Search</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <SearchResults results={results} />
     </div>
   );
 }
@@ -20,10 +25,13 @@ function Search({results}) {
 export default Search;
 
 export async function getServerSideProps(context) {
-  const useDummyData = false;
-  const data = useDummyData? Response : await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_API_kEY}&cx=${process.env.REACT_APP_CONTEXT_kEY}&q=${context.query.term}`
-  ).then((response) => response.json());
+  const useDummyData = true;
+  const startIndex = context.query.start || "0";
+  const data = useDummyData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_API_kEY}&cx=${process.env.REACT_APP_CONTEXT_kEY}&q=${context.query.term}`
+      ).then((response) => response.json());
   return {
     props: {
       results: data,
